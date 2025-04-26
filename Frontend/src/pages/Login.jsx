4a24620f987056abbf678c9e5e77a56c42eb3523
Saivarunn2004 <150151531+Saivarunn2004@users.noreply.github.com>
2025-04-26 22:05:41 +0530

    Delete Frontend directory

--- Frontend/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm';
-
-const Login = ({ onLogin }) => {
-  return <LoginForm onLogin={onLogin} />;
-};
-
-export default Login;
--- Frontend/src/utils/auth.js
+++
@@ -1,3 +0,0 @@
-export const getToken = () => {
-    return localStorage.getItem('token'); // Assuming you're saving token after login
-  };
